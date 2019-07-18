4fd28c5d9b0c3356a744484f1ccf0af8faf4b29d
Tycho van Berge Henegouwen <info@bespokeweb.nl>
2019-07-18 11:14:26 +0200

    improve nextjs example

--- example-nextjs/pages/index.js
+++ example-nextjs/pages/index.js
@@ -1,5 +1,37 @@
-function Home() {
-    return <div>Welcome to Next.js!</div>
+import React from 'react'
+import {useApiStore} from 'react-hook-api-store'
+
+const Home =  (props) => {
+    const {state, dispatch} = useApiStore()
+
+    return (
+      <div>
+          <button onClick={() => {
+              dispatch({
+                  method: 'show',
+                  route: 'todo',
+                  params: {
+                      id: 1
+                  }
+              })
+          }}>
+              get todo
+          </button>
+
+          <p>Todo  {state.todo.data.title}</p>
+
+          {props.todos.map((todo, index) => <p key={index}>{todo.title}</p>)}
+      </div>
+    )
+}
+
+Home.getInitialProps = async ({store}) => {
+    const todos = await store.dispatch({
+        method: 'index',
+        route: 'todos'
+    })
+
+    return {todos}
 }
 
 export default Home
