056f084389537c1ace915c4e236a2fc4b3a8babd
Tycho van Berge Henegouwen <info@bespokeweb.nl>
2019-07-18 11:45:11 +0200

    fix nextjs missing ApiStoreProvider

--- example-nextjs/pages/_app.js
+++ example-nextjs/pages/_app.js
@@ -1,7 +1,7 @@
 import App, {Container} from 'next/app'
 import Head from 'next/head'
 import React from 'react'
-import store, {withApiStore} from 'react-hook-api-store'
+import store, {withApiStore, ApiStoreProvider} from 'react-hook-api-store'
 
 store.router.baseUrl('https://jsonplaceholder.typicode.com')
 store.router.index('/todos', 'todos', true)
